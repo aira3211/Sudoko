4ca30afccfc86f6a3d38786107d542567095edf0
aria <aryashahbazi@gmail.com>
2021-09-08 19:23:56 +0430

    Update project

---
+++ Biuld/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Sudoku/Sudoko/tests
+# Build directory: G:/C++/AW/Sudoku/Sudoko/Biuld/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/Sudoku/Sudoko/Biuld/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/Sudoku/Sudoko/tests/CMakeLists.txt;22;add_test;G:/C++/AW/Sudoku/Sudoko/tests/CMakeLists.txt;0;")
